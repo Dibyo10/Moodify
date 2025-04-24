7b1e565aa7666a2ca1e1866c0b3514a220f75fe4
Dibyo Chakraborty <dibyo.dc@gmail.com>
2025-04-24 19:04:02 +0530

    Update database.py

--- moodify_backend_fastapi/sentiment_analysis_and_chunking/database.py
+++ moodify_backend_fastapi/sentiment_analysis_and_chunking/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from app.config import DATABASE_URL
+from sentiment_analysis_and_chunking.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
