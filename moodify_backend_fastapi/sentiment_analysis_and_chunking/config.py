e945159c409d3a51e7671b9d1a1f58c466c056b0
Dibyo Chakraborty <dibyo.dc@gmail.com>
2025-04-24 19:04:26 +0530

    Update config.py

--- moodify_backend_fastapi/sentiment_analysis_and_chunking/config.py
+++ moodify_backend_fastapi/sentiment_analysis_and_chunking/config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://dibyochakraborty@localhost:5432/moodify_db")
-OPENAI_API_KEY = os.getenv("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")
+DATABASE_URL = os.getenv("DATABASE_URL")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
