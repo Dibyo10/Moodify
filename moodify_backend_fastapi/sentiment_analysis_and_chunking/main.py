a865954ad57d8cf0b0cf17aca94934e8cee58992
Dibyo Chakraborty <dibyo.dc@gmail.com>
2025-04-24 19:03:37 +0530

    Update main.py

--- moodify_backend_fastapi/sentiment_analysis_and_chunking/main.py
+++ moodify_backend_fastapi/sentiment_analysis_and_chunking/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from app.routes import sentiment_routes, search_routes
-from app.database import engine
-from app.models import Base
+from sentiment_analysis_and_chunking.routes import sentiment_routes, search_routes
+from sentiment_analysis_and_chunking.database import engine
+from sentiment_analysis_and_chunking.models import Base
 import nltk
 import os
 
@@ -12,10 +12,10 @@ nltk.download("punkt_tab")
 
 app = FastAPI()
 
-
+# Initialize database tables
 Base.metadata.create_all(bind=engine)
 
-
+# Include API routes
 app.include_router(sentiment_routes.router, prefix="/sentiments", tags=["Sentiments"])
 app.include_router(search_routes.router, prefix="/similar-sentiments", tags=["Similarity"])
 
